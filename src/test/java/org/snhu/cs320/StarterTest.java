972dccea2139f3016a696e157fe50661310c54e4
Clinton Bush <clinton.bush@analyst1.com>
2024-05-24 21:38:31 -0400

    Removed solutions

---
+++ src/test/java/org/snhu/cs320/StarterTest.java
@@ -0,0 +1,14 @@
+package org.snhu.cs320;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class StarterTest {
+	
+	@Test
+	void test() {
+		assertTrue(1 == 1);
+	}
+	
+}
